582125fb876451018165eeabe1cbe5c3c5a845a8
ANU <sanu28751@gmail.com>
2026-02-01 18:45:44 +0530

    Time: 0 ms (100%), Space: 9 MB (21.24%) - LeetHub

---
+++ 0383-ransom-note/0383-ransom-note.c
@@ -0,0 +1,7 @@
+bool canConstruct(char* ransomNote, char* magazine) {
+    int count[26]={0};
+    for(int i=0;magazine[i];i++) count[magazine[i]-'a']++;
+    for(int i=0;ransomNote[i];i++)
+       if(--count[ransomNote[i]-'a']<0) return false;
+    return true;
+}
