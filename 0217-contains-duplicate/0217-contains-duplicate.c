560e17312fcc5b751f178efea8b931e2373653b2
ANU <sanu28751@gmail.com>
2026-02-01 19:14:41 +0530

    Time: 49 ms (49.84%), Space: 17.2 MB (87.41%) - LeetHub

---
+++ 0217-contains-duplicate/0217-contains-duplicate.c
@@ -0,0 +1,18 @@
+int Compare(const void* a,const void* b)
+{
+    int x= *(const int*)a;
+    int y= *(const int*)b;
+    return(x>y)-(x<y);
+}
+bool containsDuplicate(int* nums, int numsSize) {
+    if(numsSize<=1) return false;
+    qsort(nums,numsSize,sizeof(int),Compare);
+    for(int i=1;i<numsSize;i++)
+    {
+        if(nums[i]==nums[i-1])
+        {
+            return true;
+        }
+    }
+    return false;
+}
