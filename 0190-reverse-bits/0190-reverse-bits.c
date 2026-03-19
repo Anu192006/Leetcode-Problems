f3144b482b59abd6e3a76c2c1919c1050abcef7c
ANU <sanu28751@gmail.com>
2026-03-19 19:58:53 +0530

    Time: 2 ms (49.75%), Space: 9 MB (8.33%) - LeetHub

---
+++ 0190-reverse-bits/0190-reverse-bits.c
@@ -0,0 +1,11 @@
+int reverseBits(int n) {
+    unsigned int result=0;
+    unsigned int num=(unsigned int)n;
+    for(int i=0;i<32;i++)
+    {
+        result<<= 1;
+        result|=(num&1);
+        num>>= 1;
+    }
+    return (int)result;
+}
