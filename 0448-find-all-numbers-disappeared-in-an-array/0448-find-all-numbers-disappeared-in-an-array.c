397d090801620481c962fa4d4533ae33f379fb93
ANU <sanu28751@gmail.com>
2026-03-20 11:02:53 +0530

    Time: 2 ms (53.51%), Space: 35.8 MB (59.47%) - LeetHub

---
+++ 0448-find-all-numbers-disappeared-in-an-array/0448-find-all-numbers-disappeared-in-an-array.c
@@ -0,0 +1,22 @@
+/**
+ * Note: The returned array must be malloced, assume caller calls free().
+ */
+int* findDisappearedNumbers(int* nums, int numsSize, int* returnSize) {
+    int* result=(int*)malloc(numsSize* sizeof(int));
+    int index=0;
+    for(int i=0;i<numsSize;i++)
+    {
+        int val=abs(nums[i])-1;
+        if(nums[val]>0){
+            nums[val]= -nums[val];
+        }
+    }
+    for(int i=0;i<numsSize;i++)
+    {
+        if(nums[i]>0){
+            result[index++]=i+1;
+        }
+    }
+    *returnSize=index;
+    return result;
+}
