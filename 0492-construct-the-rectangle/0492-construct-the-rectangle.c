10189c6c863e798c1dbbaa7ca986a23bf50a3e64
ANU <sanu28751@gmail.com>
2026-03-21 13:57:26 +0530

    Time: 0 ms (100%), Space: 8.5 MB (80.61%) - LeetHub

---
+++ 0492-construct-the-rectangle/0492-construct-the-rectangle.c
@@ -0,0 +1,15 @@
+/**
+ * Note: The returned array must be malloced, assume caller calls free().
+ */
+int* constructRectangle(int area, int* returnSize) {
+    int* result=(int*)malloc(2* sizeof(int));
+    *returnSize=2;
+    int w=sqrt(area);
+    while(area%w!=0)
+    {
+        w--;
+    }
+    result[0]=area/w;
+    result[1]=w;
+    return result;
+}
