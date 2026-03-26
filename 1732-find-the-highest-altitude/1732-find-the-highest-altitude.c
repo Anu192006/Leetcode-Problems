e13b56b28ace1a6357a09f44193d738c85c06d7b
ANU <sanu28751@gmail.com>
2026-03-26 21:28:08 +0530

    Time: 0 ms (100%), Space: 9 MB (10.78%) - LeetHub

---
+++ 1732-find-the-highest-altitude/1732-find-the-highest-altitude.c
@@ -0,0 +1,12 @@
+int largestAltitude(int* gain, int gainSize) {
+    int cur=0;
+    int max=0;
+    for(int i=0;i<gainSize;i++)
+    {
+        cur+=gain[i];
+        if(cur>max){
+            max=cur;
+        }
+    }
+    return max;
+}
