254e21be7e59d68c7e4c167d2d605305a4cefd0a
ANU <sanu28751@gmail.com>
2026-03-26 11:16:20 +0530

    Time: 0 ms (100%), Space: 14.4 MB (47.29%) - LeetHub

---
+++ 0041-first-missing-positive/0041-first-missing-positive.c
@@ -0,0 +1,23 @@
+int firstMissingPositive(int* nums, int numsSize) {
+    for(int i=0;i<numsSize;i++)
+    {
+        if(nums[i]<=0)
+        {
+            nums[i]=numsSize+2;
+        }
+    }
+    for(int i=0;i<numsSize;i++)
+    {
+        int val=abs(nums[i]);
+        if(val>=1 && val<=numsSize){
+            nums[val-1]=abs(nums[val-1])*-1;
+        }
+    }
+    for(int i=0;i<numsSize;i++)
+    {
+        if(nums[i]>0){
+            return i+1;
+        }
+    }
+    return numsSize+1;
+}
