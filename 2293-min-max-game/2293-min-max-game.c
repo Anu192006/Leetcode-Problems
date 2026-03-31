d7e382447ec2733a81c2841ba820bca698b1ee79
ANU <sanu28751@gmail.com>
2026-03-31 22:53:16 +0530

    Time: 0 ms (100%), Space: 8.9 MB (15.56%) - LeetHub

---
+++ 2293-min-max-game/2293-min-max-game.c
@@ -0,0 +1,14 @@
+int minMaxGame(int* nums, int numsSize) {
+    while(numsSize>1){
+        for(int i=0;i<numsSize/2;i++)
+        {
+            if(i%2==0){
+                nums[i]=nums[2*i]<nums[2*i+1]?nums[2*i]:nums[2*i+1];
+            }else{
+                nums[i]=nums[2*i]>nums[2*i+1]?nums[2*i]:nums[2*i+1];
+            }
+        }
+        numsSize/=2;
+    }
+    return nums[0];
+}
